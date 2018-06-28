0cfc17cf8b1d8b01459cde7975a07b5e6fafb082
Chamroeun UON <chamroeunuon@gmail.com>
2018-06-28 11:20:38 +0700

    RenameVehicleFeaturs

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -3,10 +3,10 @@ using ASP_Angular.Models;
 using Microsoft.EntityFrameworkCore;
 namespace ASP_Angular.Persistence {
     public class VegaDbContext : DbContext {
-        public DbSet<Make> Makes { get; set; }
+         public DbSet<Make> Makes { get; set; }
         public DbSet<Model> Models { get; set; }
-        public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Feature> Features { set; get; }
+        public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<VehicleFeature> VehicleFeatures { get; set; }
         
 
