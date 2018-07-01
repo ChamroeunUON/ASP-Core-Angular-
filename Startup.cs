805b293e9161abd28c69a0c50515c95e587ff5fc
Chamroeun UON <chamroeunuon@gmail.com>
2018-07-01 08:55:20 +0700

    Fix bug IunitofWork Dependency Star

--- Startup.cs
+++ Startup.cs
@@ -11,58 +11,49 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ASP_Angular
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            
+namespace ASP_Angular {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
+
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {   
-            services.AddScoped<IVehicleRepository,VehicleRepository>();
-            services.AddScoped<IUnitOfWork,IUnitOfWork>();
-            services.AddAutoMapper();
-            services.AddMvc();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddScoped<IUnitOfWork, UnitofWork> ();
+            services.AddScoped<IVehicleRepository, VehicleRepository> ();
+
+            services.AddAutoMapper ();
+            services.AddMvc ();
             // var connection = @"Server=(localdb)\mssqllocaldb;Database=VegaDB;Trusted_Connection=True;ConnectRetryCount=0";
-            
-            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefualtConnection")));
+
+            services.AddDbContext<VegaDbContext> (options => options.UseSqlServer (Configuration.GetConnectionString ("DefualtConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseWebpackDevMiddleware (new WebpackDevMiddlewareOptions {
                     HotModuleReplacement = true
                 });
-            }
-            else
-                {
-                app.UseExceptionHandler("/Home/Error");
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
             }
 
-            app.UseStaticFiles();
+            app.UseStaticFiles ();
 
-            app.UseMvc(routes =>
-            { 
-                routes.MapRoute(
+            app.UseMvc (routes => {
+                routes.MapRoute (
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
 
-                routes.MapSpaFallbackRoute(
+                routes.MapSpaFallbackRoute (
                     name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
+                    defaults : new { controller = "Home", action = "Index" });
             });
         }
     }
