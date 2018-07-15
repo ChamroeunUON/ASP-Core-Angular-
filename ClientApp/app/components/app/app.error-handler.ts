964e29bfca7983a2985ec2623242825702a7ace4
Chamroeun UON <chamroeunuon@gmail.com>
2018-07-15 17:24:59 +0700

    Check isDevMode

--- ClientApp/app/components/app/app.error-handler.ts
+++ ClientApp/app/components/app/app.error-handler.ts
@@ -1,13 +1,15 @@
 import * as Raven from 'raven-js';
 import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler, Inject, NgZone } from '@angular/core';
+import { ErrorHandler, Inject, NgZone,isDevMode } from '@angular/core';
 
 export class AppErrorHandler implements ErrorHandler {
     constructor(
         @Inject(ToastyService) private toastyService: ToastyService,
         @Inject(NgZone) private ngZone : NgZone) { }
     handleError(error: any): void {
-        Raven.captureException(error.originalError || error);
+        if(!isDevMode())
+            Raven.captureException(error.originalError || error);
+        else throw error;
         this.ngZone.run(():any => {
             this.toastyService.error({
                 title: "Opp!!",
